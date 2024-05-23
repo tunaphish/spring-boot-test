7bece89f6be87f0f66c57edddfef1f6b2bd3c045
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2024-05-23 17:50:00 -0400

    init commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use an official OpenJDK runtime as a parent image
+FROM openjdk:17-jdk-alpine
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the application's JAR file to the container
+COPY target/test.project-0.0.1-SNAPSHOT.jar app.jar
+
+# Expose the port the application runs on
+EXPOSE 8080
+
+# Run the JAR file
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/tunaphish/test/project/Application.java
@@ -0,0 +1,28 @@
+package com.tunaphish.test.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/messages")
+	public String getMessage() {
+		return "Hello from Docker!";
+	}
+
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
